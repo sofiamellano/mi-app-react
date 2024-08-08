2edca2460aec5d5c0464a5b9410d095c9f772cc2
sofiamellano <sofiimellano@gmail.com>
2024-08-08 20:52:38 -0300

    useEffect

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,24 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import Container from './components/Container';
 import Titulo from './components/Titulo'; 
 import Formulario from './components/Formulario';
 
 function App() {
 
-  const [tasks, setTasks] = useState([]);
+  console.log('Componente App ejecutado');
+
+  const [tasks, setTasks] = useState([]); //estado arranque con el valor que esta en localstorage
 
   const onSubmitHandler = (event) => {
     event.preventDefault();
     if(!event.target.elements.taskName.value) return;
     setTasks([...tasks, event.target.elements.taskName.value]);
   }
+
+  useEffect( () => {
+    console.log('useEffect ejecutado', [tasks]);
+    //Tarea: Agregar el guardado en el localStorage, actualizar tasks en localstorage
+  }, [tasks] ); //array = dependencias // dentro del useeffect no puede ir una dependencia del mismo
 
   return (
     <>
