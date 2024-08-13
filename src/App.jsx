d6d943083221711f7d49f8509c46427a2b514830
sofiamellano <sofiimellano@gmail.com>
2024-08-13 10:15:55 -0300

    tarea hecha

--- src/App.jsx
+++ src/App.jsx
@@ -7,21 +7,24 @@ function App() {
 
   console.log('Componente App ejecutado');
 
-  const [tasks, setTasks] = useState([]); //estado arranque con el valor que esta en localstorage
+  const [tasks, setTasks] = useState(() => { const savedTasks = localStorage.getItem('tasks');
+    return savedTasks ? JSON.parse(savedTasks) : []; }); //estado arranque con el valor que esta en localstorage
 
   const onSubmitHandler = (event) => {
     event.preventDefault();
-    if(!event.target.elements.taskName.value) return;
-
+    //if(!event.target.elements.taskName.value) return;
+    const taskName = event.target.elements.taskName.value;
+    if (!taskName) return;
+    const newTask = { name: taskName, completed: false };
     //reemplazar el string del value por un objeto que contenga el string y el estado finalizado.
-    setTasks([...tasks, event.target.elements.taskName.value]);
+    setTasks([...tasks, newTask]);
   }
 
   useEffect( () => {
     console.log('useEffect ejecutado', [tasks]);
-    //Tarea: Agregar el guardado en el localStorage, actualizar tasks en localstorage
+    localStorage.setItem('tasks',JSON.stringify(tasks));
   }, [tasks] ); //array = dependencias // dentro del useeffect no puede ir una dependencia del mismo
-
+//Tarea: Agregar el guardado en el localStorage, actualizar tasks en localstorage
   return (
     <>
     <Container>
